b54cb276c46b4315576bef453af6a584efd41258
martin-simeonov <simeonovelsys@gmail.com>
2016-01-26 12:16:07 +0200

    Task 1 updated

--- solutions/01.rb
+++ solutions/01.rb
@@ -1,11 +1,15 @@
+EXCHANGE_RATE = {
+  usd: 1.7408,
+  eur: 1.9557,
+  gbp: 2.6415,
+  bgn: 1,
+}
+
 def convert_to_bgn(price, currency)
-  price *= 1.7408 if currency == :usd
-  price *= 1.9557 if currency == :eur
-  price *= 2.6415 if currency == :gbp
-  price.round(2)
+  (price * EXCHANGE_RATE[currency]).round(2)
 end
 
 def compare_prices(price_one, currency_one, price_two, currency_two)
-  result = convert_to_bgn(price_one, currency_one)
-  result -= convert_to_bgn(price_two, currency_two)
+  first_price_in_bgn = convert_to_bgn(price_one, currency_one)
+  first_price_in_bgn - convert_to_bgn(price_two, currency_two)
 end
